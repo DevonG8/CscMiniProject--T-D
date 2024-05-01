14f03ad6b5bcd3901711398cfcfee49a93074cb2
Devon Gay <Devongay132@gmail.com>
2024-05-01 14:59:16 -0400

    Changes to be committed: 	new file:   Player.java

---
+++ Player.java
@@ -0,0 +1,11 @@
+public class Player {
+    private int number;
+    private int currentRoom;
+    private boolean toolsCollected;
+    private int lastMachinePartCollected;
+
+    public Player(int number, int room){
+        this.number = number;
+        this.currentRoom = room;
+    }
+}
