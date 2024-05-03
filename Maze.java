c53d1422a2803b9c326b7170bc4d2454e4e43d66
Antonio Cavanaugh Lillo <antonio.armati.cl@gmail.com>
2024-05-03 14:45:19 -0400

    Created Maze.java class, psuedo code instruction for later

---
+++ Maze.java
@@ -0,0 +1,33 @@
+/**
+ * "This is the main class that will either:
+ * 
+ * - extend JavaFx Application class, builds the GUI and a set of nested classes
+ * to handle the ActionEvent of each the buttons the player can click 
+ * OR
+ * - Read player text commands then interpret them
+ * 
+ * Either way, this class will instaniate the Game class, call the different
+ * methods in the currentPlayer object (currrentPlayer is part of Game) to advance the game.
+ * It should also display to the player the message it is receiving from these methods"
+ * 
+ * 
+ */
+
+//Going to need javafx import references and dependencies
+
+
+public class Maze extends Application {
+    
+}
+
+//Needs a GUI builder
+
+
+//Maybe error handling?
+
+
+
+//launch the game
+public static void main(String[] args) {
+    launch(args);
+}
