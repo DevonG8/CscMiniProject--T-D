1c5021df27abb1064d31104521e275314d9dceec
Antonio Cavanaugh Lillo <antonio.armati.cl@gmail.com>
2024-05-03 14:34:25 -0400

    Creating Workshop class, +  temp. comments of psuedo code for implementation.

---
+++ Workshop.java
@@ -0,0 +1,16 @@
+/**
+ * This class, Workshop, will extend the Room class and will be the room
+ * in which the workshop exists in the game for the players to build the
+ * machine. 
+ * 
+ * @author Antonio Cavanaugh Lillo
+ * @version 0.0
+ * 
+ */
+public class Workshop {
+
+    //argument constructor method, room # as parameter
+
+    //isWorkshop() method overriding isWorkshop of Room returning true
+    
+}
