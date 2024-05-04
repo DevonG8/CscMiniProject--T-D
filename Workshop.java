78ed08cc1c17a60c51ff7de9b9ba620269174966
Antonio Cavanaugh Lillo <antonio.armati.cl@gmail.com>
2024-05-04 10:38:17 -0400

    Implemented Workshop  logic

--- Workshop.java
+++ Workshop.java
@@ -4,13 +4,24 @@
  * machine. 
  * 
  * @author Antonio Cavanaugh Lillo
- * @version 0.0
+ * @version 1.0
+ * 
  * 
  */
-public class Workshop {
+public class Workshop extends Room {
 
     //argument constructor method, room # as parameter
 
+    public Workshop(int number) {
+        //inherits room number
+         super(number);
+    }
+
+    //This method is what makes this room 'contain' the workshop in game.
     //isWorkshop() method overriding isWorkshop of Room returning true
+    @Override
+    public boolean isWorkshop() {
+        return true;
+    }
     
 }
