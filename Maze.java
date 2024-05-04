651bce5114ebb7784091ffa7f5912f2b1d12725f
Antonio Cavanaugh Lillo <antonio.armati.cl@gmail.com>
2024-05-04 17:46:52 -0400

    Started adding some implementation for Maze.java

--- Maze.java
+++ Maze.java
@@ -16,18 +16,62 @@
 //Going to need javafx import references and dependencies
 
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
 public class Maze extends Application {
+
+    private Game game;
+    private GridPane gridPane;
+    pr
+
+    private Button upButton;
+    private Button leftButton;
+    private Button rightButton;
+    private Button downButton;
+    private Button collectPart;
+    private Button collectTools;
+    private Button buildMachine;
+
+    private Button helpButton;
+    private Button restartButton;
+
+    private TextField textMessages;
+    private TextField textInput;
+
     
-}
+    public void start(Stage applicationStage) throws Exception {
+
+
+        Scene scene = new Scene();
+        applicationStage.setScene(scene);
+        applicationStage.setTitle("Maze");
+        applicationStage.show();
+    }
+    
+
 
 //Needs a GUI builder
+
 
 
 //Maybe error handling?
 
 
 
-//launch the game
-public static void main(String[] args) {
-    launch(args);
+    //launch the game
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
