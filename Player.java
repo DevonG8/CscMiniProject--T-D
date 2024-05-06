8818ab8b1087337ae9355b88b85beeb96ea911a0
Devon Gay <Devongay132@gmail.com>
2024-05-06 14:39:54 -0400

    Changes to be committed: 	modified:   Player.java

--- Player.java
+++ Player.java
@@ -2,10 +2,39 @@ public class Player {
     private int number;
     private Room currentRoom;
     private boolean toolsCollected;
-    private int lastMachinePartCollected;
+    private Part lastMachinePartCollected;
 
-    public Player(int number, Room currentRoom){
+    public Player(int number, Room room){
         this.number = number;
-        this.currentRoom = currentRoom;
+        this.currentRoom = room;
+    }
+    public Part lastMachinePartCollected(){
+        return lastMachinePartCollected;
+    }
+    public Room currentRoom(){
+        return currentRoom;
+    }
+
+    public boolean hasTools(){
+        return toolsCollected;
+    }
+
+    public String move(int Direction){
+        //create new direction object
+        Room nnewRoom = currentRoom.getDoor(Direction);
+        //takes directions
+
+
+        //If there is a door change currentRoom 
+        //accordingly and returns string
+
+        //otherwise, it returns "no door in this direction"
+    }
+    public String collectPart(){
+        //check if possible to collect part
+
+        //if can collect set boolean true adn return string
+
+        //otherwise return different string 
     }
 }
