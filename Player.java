f4f4cd36b7d63554ea018e36a9b06bce0068e691
Devon Gay <Devongay132@gmail.com>
2024-05-08 19:17:09 -0400

    Changes to be committed: 	modified:   Player.java

--- Player.java
+++ Player.java
@@ -1,3 +1,8 @@
+/**
+ * This class represents a player and manages
+ * a players moves and status in the game
+ * @author Devon Gay
+ */
 public class Player {
     private int number;
     private Room currentRoom;
@@ -8,33 +13,55 @@ public class Player {
         this.number = number;
         this.currentRoom = room;
     }
+    /**
+     * getter method for lsatMachinePartCollected
+     * @return String 
+     */
     public Part lastMachinePartCollected(){
         return lastMachinePartCollected;
     }
+
+    /**
+     * getter method for currentRoom
+     * @return String 
+     */
     public Room currentRoom(){
         return currentRoom;
     }
 
+    /**
+     * a getter method for toolsCollected boolean
+     * @return boolean
+     */
     public boolean hasTools(){
         return toolsCollected;
     }
 
+    /**
+     * 
+     * @param Direction
+     * @return
+     */
     public String move(int Direction){
-        //create new direction object
-        Room nnewRoom = currentRoom.getDoor(Direction);
-        //takes directions
+        //new room
+        Room newRoom = currentRoom.getDoor(Direction);
 
-
-        //If there is a door change currentRoom 
-        //accordingly and returns string
-
-        //otherwise, it returns "no door in this direction"
+        //if new room not there print meassge else put player in new room and print message 
+        if(newRoom == null){
+            return ("There is no room in this direction");
+        }else{  
+            currentRoom = newRoom;
+            return currentRoom.printMessage();
+        }
     }
-    public String collectPart(){
-        //check if possible to collect part
+    // public String collectPart(){
+    //     //check if possible to collect part
+    //     if(currentRoom.hasPart == null){
 
-        //if can collect set boolean true adn return string
+    //     }
 
-        //otherwise return different string 
-    }
+    //     //if can collect set boolean true adn return string
+
+    //     //otherwise return different string 
+    // }
 }
